c3a1c4610095f746148e33943dcc6865cc4cbd9d
Flávio Lourenço <contato.flaviolourenco@gmail.com>
2026-06-06 00:03:12 -0300

    working autocomplete with sub dirs

--- lib/shconf-autocomplete.sh
+++ lib/shconf-autocomplete.sh
@@ -3,7 +3,13 @@ _shconf_completion() {
 
     cur="${COMP_WORDS[COMP_CWORD]}"
 
-    COMPREPLY=($(compgen -W "$(shconf __complete)" -- "$cur"))
+    local args=("${COMP_WORDS[@]:1:$((COMP_CWORD-1))}")
+
+    COMPREPLY=(
+        $(compgen -W "$(shconf __complete "${args[@]}")" -- "$cur")
+    )
+
+    # COMPREPLY=($(compgen -W "$(shconf __complete)" -- "$cur"))
 }
 
 complete -F _shconf_completion shconf
